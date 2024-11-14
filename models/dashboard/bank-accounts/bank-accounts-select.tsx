e88db26f141b4df06f110aaa1ee526c4a7c20b83
mykolatsiapalo1 <mykolatsiapalo1@gmail.com>
2024-11-14 14:45:24 +0200

    fix: build

--- models/dashboard/bank-accounts/bank-accounts-select.tsx
+++ models/dashboard/bank-accounts/bank-accounts-select.tsx
@@ -2,19 +2,35 @@ import { Select } from "@/components/ui/select";
 import { SkDiv } from "@/components/ui/sk-div";
 import { Account } from "@/types/accounts";
 import { SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-export function BankAccountsSelect({ data }: { data?: Account[] }) {
+import { useMemo } from "react";
+export function BankAccountsSelect({ data, selectedAccountId }: { data?: Account[]; selectedAccountId: string }) {
+  const selectedAccount = useMemo(() => data?.find((account) => account.id === selectedAccountId), [selectedAccountId]);
   return (
     <SkDiv isLoading={!data?.length}>
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Theme" />
+      <span className="mb-2 text-base font-normal leading-normal text-primary">Select account</span>
+      {/* <Select>
+        <SelectTrigger className="mt-2 w-[100%] text-base">
+          <SelectValue
+            placeholder={<SelectItemRow {...selectedAccount} />}
+          />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
+          {data?.map((account) => (
+            <SelectItem value={account.id}>
+              <SelectItemRow {...account} />
+            </SelectItem>
+          ))}
         </SelectContent>
-      </Select>
+      </Select> */}
     </SkDiv>
+  );
+}
+
+function SelectItemRow(account: Account) {
+  return (
+    <div>
+      <span>{account.complete_name}</span>
+      <span className="text-gray-400"> - {account.identification_external_uuid}</span>
+    </div>
   );
 }
